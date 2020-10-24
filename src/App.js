2b1c9f41c74a4b06b1f2e88649e272bd88f3a263
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-10-24 11:24:49 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import FunOne from './components/functionalComponent_1';
 import FunTwo from './components/functionalComponent_2';
+import Class from './components/classComponent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>App Component!</h1>
       <FunOne/>
       <FunTwo/>
+      <Class/>
     </div>
   );
 }
---
+++ src/components/classComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class classComponent extends Component{
+    render(){
+        return <h1>Class Component!</h1>
+    }
+}
+
+export default classComponent;
