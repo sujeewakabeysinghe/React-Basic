820f363f642482e9c516aa96c39ed0847c482939
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-10-29 16:40:40 +0530

    event binding

---
+++ src/components/eventBinding.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react' //rce
+
+class eventBinding extends Component {
+
+    constructor(props) { //rconst
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default eventBinding
