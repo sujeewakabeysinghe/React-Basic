c7d55e38f8326f17bde3b0aaff695702746ec8d7
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-11-01 14:11:27 +0530

    conditional rendering

---
+++ src/components/conditionalRendering.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class conditionalRendering extends Component {
+    //element variable approach
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default conditionalRendering
