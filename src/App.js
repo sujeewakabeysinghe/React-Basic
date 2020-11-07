9c25306d3aded1c631f17ae9847de0be90acb608
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-11-07 23:27:34 +0530

    removed comment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import './App.css';
-// import FunOne from './components/functionalComponent_1';
-// import FunTwo from './components/functionalComponent_2';
-// import Class from './components/classComponent';
-// import PropsOne from './components/props_1';
-// import PropsTwo from './components/props_2';
-// import PropsThree from './components/props_3';
-// import StateOne from './components/state_1';
-// import StateTwo from './components/state_2';
-// import EventOne from './components/event_1';
-// import EventTwo from './components/event_2';
-// import EventBindingOne from './components/eventBinding_1';
-// import EventBindingTwo from './components/eventBinding_2';
-// import ParentComponent from './components/parentComponent';
-// import Condition from './components/conditionalRendering';
-// import ListOne from './components/listRendering_1';
-// import ListTwo from './components/listRendering_2';
-// import ListThree from './components/listRendering_3';
-// import Style from './components/style';
-// import StyleInline from './components/styleInline';
-// import Form from './components/nextTen/form';
+import FunOne from './components/functionalComponent_1';
+import FunTwo from './components/functionalComponent_2';
+import Class from './components/classComponent';
+import PropsOne from './components/props_1';
+import PropsTwo from './components/props_2';
+import PropsThree from './components/props_3';
+import StateOne from './components/state_1';
+import StateTwo from './components/state_2';
+import EventOne from './components/event_1';
+import EventTwo from './components/event_2';
+import EventBindingOne from './components/eventBinding_1';
+import EventBindingTwo from './components/eventBinding_2';
+import ParentComponent from './components/parentComponent';
+import Condition from './components/conditionalRendering';
+import ListOne from './components/listRendering_1';
+import ListTwo from './components/listRendering_2';
+import ListThree from './components/listRendering_3';
+import Style from './components/style';
+import StyleInline from './components/styleInline';
+import Form from './components/nextTen/form';
 import LifeCycleA from './components/nextTen/lifeCycleA'
 
 function App() {
   return (
     <div className="App">
-      {/* <h1>App Component!</h1>
+      <h1>App Component!</h1>
       <FunOne/>
       <FunTwo/>
       <Class/>
@@ -48,7 +48,7 @@ function App() {
       <Style/> without props regular way
       <Style className={true}/>
       <StyleInline/> 
-      <Form/> */}
+      <Form/>
       <LifeCycleA/>
     </div>
   );
