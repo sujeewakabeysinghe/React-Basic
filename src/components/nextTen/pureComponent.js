161bc8e27b7c7c09b37b9b4aab35b454e655679e
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-12-29 21:42:02 +0530

    pure component

---
+++ src/components/nextTen/pureComponent.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react' //rpce - to create a pure component
+
+export class pureComponent extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default pureComponent
