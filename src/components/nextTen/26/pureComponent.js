9f3d5ccbbadce7a0c91ea4949380cb6dcd6f2bc7
sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2020-12-29 21:45:50 +0530

    pure component

--- src/components/nextTen/26/pureComponent.js
+++ src/components/nextTen/26/pureComponent.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react' //rpce - to create a pure component
 
-export class pureComponent extends PureComponent {
+class pureComponent extends PureComponent {
     render() {
         return (
             <div>
